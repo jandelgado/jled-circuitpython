7cd1b81fd7b8a5cdadd257732390ef3b4f4d014e
Jan Delgado <jdelgado@gmx.net>
2023-01-09 23:35:25 +0100

    minimize invocations of mpremote command

--- scripts/install_mp.sh
+++ scripts/install_mp.sh
@@ -1,31 +1,47 @@
 #!/bin/bash
 #
-# compile JLed to binary .mpy format and copy lib and example
+# compile JLed to binary .mpy format and copy lib and example files
 # to micropython device using mpremote. Depends on:
-# - mpy-cross (https://pypi.org/project/mpy-cross)
 # - mpremote (https://pypi.org/project/mpremote/
+# - mpy-cross (https://pypi.org/project/mpy-cross)
 #
+# Due to stability problems with mpremote and my ESP32, files are collected
+# in a temp directory and then copied with on mpremote cp invocation.
+#
+# Environment variables:
+#   MAIN_SOURCE - path to to file to copy as main.py (e.g. an example script)
+#   MPREMOTE - mpremote binary to use
 set -eouT pipefail
 
 # example to copy
-EXAMPLE_SOURCE="examples/jled_micropython.py"
+MAIN_SOURCE=${MAIN_SOURCE:-examples/jled_micropython.py}
+MPREMOTE=${MPREMOTE:-mpremote}
 
 SCRIPT_DIR=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
 cd "$SCRIPT_DIR/.."
 
-LIB_SOURCES="jled/__init__.py jled/jled.py jled/hal_pwm_micropython.py jled/hal_time_micropython.py"
-SOURCES="$LIB_SOURCES $EXAMPLE_SOURCE"
+LIB_SOURCES="jled/__init__.py jled/jled.py jled/hal_pwm_micropython.py jled/hal_time_micropython.py jled/play.py jled/jled_sequence.py"
+SOURCES="$LIB_SOURCES $MAIN_SOURCE"
+
+echo $MPREMOTE
 
 echo ">> compiling .py to .mpy..."
 for file in $SOURCES; do
     mpy-cross "$file"
 done
 
-echo ">> copy lib files to device..."
-mpremote mkdir :jled 2>&1 > /dev/null || true
+# prepare local directory which will be uploaded 
+SRC=$(mktemp -d)
+trap "rm -rf $SRC" EXIT
+mkdir $SRC/jled
+
+echo ">> prepare upload folder..."
 for file in $LIB_SOURCES; do
-    mpremote cp ${file%.py}.mpy :jled/
+    cp ${file%.py}.mpy $SRC/jled
 done
 
-echo ">> copy example $EXAMPLE_SOURCE"
-mpremote cp ${EXAMPLE_SOURCE%.py}.mpy :main.mpy
+# compiled main.mpy is not supported currently
+cp $MAIN_SOURCE $SRC/main.py
+
+cd $SRC && ${MPREMOTE} cp -r . :
+
